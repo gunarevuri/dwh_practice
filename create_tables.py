283e6d8afbc6a858c35c9f86fefeb9037cc0582c
gunarevuri <gunarevuri@gmail.com>
2020-07-20 12:59:09 +0530

    added create_tables.py

---
+++ create_tables.py
@@ -0,0 +1,51 @@
+import configparser
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries, insert_table_queries, select_number_rows_queries
+
+
+def drop_tables(cur, conn):
+    for query in drop_table_queries:
+        print("query"+str(query))
+        cur.execute(query)
+        conn.commit()
+        print("finish--------------------finish")
+
+
+
+def create_tables(cur, conn):
+    for query in create_table_queries:
+        print("query"+str(query))
+        cur.execute(query)
+        conn.commit()
+        print("finish--------------------finish")
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+# def count_rows():
+#     config = configparser.ConfigParser()
+#     config.read('dwh.cfg')
+
+#     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+#     cur = conn.cursor()
+    
+#     for query in select_number_rows_queries:
+#         print("query"+str(query))
+#         cur.execute(query)
+#         conn.commit()
+
+#     conn.close()
+    
+if __name__ == "__main__":
+    main()
+#     count_rows()
